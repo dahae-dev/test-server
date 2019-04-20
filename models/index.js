592cd05f6a17402562224250bb5d788211f00e89
dahaekim88 <dhkim1014@gmail.com>
2019-04-20 11:32:36 +0900

    Fix typo

--- models/index.js
+++ models/index.js
@@ -5,17 +5,17 @@ const db = {};
 
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
-// sequelize
-//   .authenticate()
-//   .then(() => {
-//     console.log("Connection has been established successfully.");
-//   })
-//   .catch(err => {
-//     console.error("Unable to connect to the database:", err);
-//   });
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch(err => {
+    console.error("Unable to connect to the database:", err);
+  });
 
-db.Author = require("./Author")(sequelize, Sequelize);
-db.Post = require("./Post")(sequelize, Sequelize);
+db.Author = require("./author")(sequelize, Sequelize);
+db.Post = require("./post")(sequelize, Sequelize);
 
 db.Author.hasMany(db.Post, { foreignKey: "authorId", sourceKey: "id" });
 db.Post.belongsTo(db.Author, { foreignKey: "authorId", targetKey: "id" });
